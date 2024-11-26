f9ae6e252d46bfd63c61654893c41a08c169018a
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-11-26 22:35:32 +0800

    find_length

---
+++ 1) arrays/two_pointers/find_length.py
@@ -0,0 +1,11 @@
+def find_length(nums, k):
+    
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+    
+    return ans
